c826e311b698f51cc12073b7ec4418b94ebdff5e
Thomas Göllner <tgoellner@ujam.com>
2019-10-23 15:45:06 +0200

    updated groovy script

--- dropbox.groovy
+++ dropbox.groovy
@@ -2,29 +2,29 @@ def upload(sourceFile, destinationFolder, wipeFolder = false) {
     def downloadLink = ""
 
     if( ! ( env.DROPBOX_ACCESSTOKEN && env.DROPBOX_ACCESSTOKEN != "" ) ) {
-        println "\nNOTE: Dropbox uploader skipped due to missing Access Token.\n"
+        echo "\nNOTE: Dropbox uploader skipped due to missing Access Token.\n"
         return downloadLink;
     }
 
     if( ! ( env.DROPBOX_UPLOADER_PATH && env.DROPBOX_UPLOADER_PATH != "" ) ) {
-        println "\nNOTE: Dropbox uploader path is not set.\n"
+        echo "\nNOTE: Dropbox uploader path is not set.\n"
         return downloadLink;
     }
 
     if( ! fileExists(env.DROPBOX_UPLOADER_PATH + '/dropbox_uploader.sh') ) {
-        println "\nNOTE: Dropbox uploader script does not exist.\n"
+        echo "\nNOTE: Dropbox uploader script does not exist.\n"
         return downloadLink;
     }
 
     if( ! fileExists("${sourceFile}") ) {
-        println "\nNOTE: Dropbox uploader skipped due to source file ${sourceFile} not found.\n"
+        echo "\nNOTE: Dropbox uploader skipped due to source file ${sourceFile} not found.\n"
         return downloadLink;
     }
 
     def db_cmd = "${env.DROPBOX_UPLOADER_PATH}/dropbox_uploader.sh -q -t \"${env.DROPBOX_ACCESSTOKEN}\"";
 
     def baseSourceName = sh (script: "basename ${sourceFile}", returnStdout: true).trim()
-    println "\nNOTE: Uploading ${baseSourceName} to Drop Box...\n"
+    echo "\nNOTE: Uploading ${baseSourceName} to Drop Box...\n"
 
     if( wipeFolder ) {
         // make sure we have no macOS build folder on DropBox...
@@ -50,7 +50,7 @@ def upload(sourceFile, destinationFolder, wipeFolder = false) {
         sh "${db_cmd} upload \"${sourceFile}\" \"${destinationFolder}/${baseSourceName}\""
         downloadLink = sh (script: "${db_cmd} share \"${destinationFolder}/${baseSourceName}\"", returnStdout: true).trim()
     } catch (err) {
-        println "\nERROR: Dropbox uploader could not upload ${sourceFile} build to dropbox:/${destinationFolder}/${baseSourceName} and create a share link.\n"
+        echo "\nERROR: Dropbox uploader could not upload ${sourceFile} build to dropbox:/${destinationFolder}/${baseSourceName} and create a share link.\n"
         downloadLink = ""
     }
 
